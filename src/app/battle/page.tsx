fde6e6f8e1a076b0537e9d429c8c1cdeff0f4f73
Maximilian Paju <maximilian.paju@gmail.com>
2023-11-02 11:27:25 +0100

    feat: battle actions

--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -4,15 +4,19 @@ import { useState } from "react";
 import { Alignment, Clazz, Race } from "@/lib/dnd/character/character";
 import {
   Battle,
-  BattleAction,
   BattleCharacter,
-  BattleCharacterAction,
+  BattleEvent,
   BattleRound,
-  characterHasSpentAction,
+  characterFinished,
+  characterHasFinishedRound,
+  characterHasSpentBonusAction,
+  characterHasSpentPrimaryAction,
+  characterPerformBonus,
+  getRoundEvents,
 } from "@/lib/dnd/battle/battle";
 import { getCharacterInitiative } from "@/lib/dnd/character/character-stats";
 import {
-  characterAttack,
+  characterPerformAttack,
   currentCharacterTurn,
   hasRolledForInitiative,
 } from "@/lib/dnd/battle/battle";
@@ -20,18 +24,34 @@ import {
 export default function Battle() {
   const [battle, setBattle] = useState<Battle>({
     characters: [],
-    rounds: [
-      {
-        characterActions: [],
-      },
-    ],
+    events: [],
+    rounds: [{ id: "round-1" }],
   });
 
-  function performAttack(battle: Battle, character: BattleCharacter) {
-    const b = characterAttack(battle, character.character.id);
+  function performBonus(battle: Battle, character: BattleCharacter) {
+    const b = characterPerformBonus(battle, character.character.id);
     setBattle((old) => ({
       characters: old.characters,
       rounds: [...b.rounds],
+      events: [...b.events],
+    }));
+  }
+
+  function finishRound(battle: Battle, character: BattleCharacter) {
+    const b = characterFinished(battle, character.character.id);
+    setBattle((old) => ({
+      characters: old.characters,
+      rounds: [...b.rounds],
+      events: [...b.events],
+    }));
+  }
+
+  function performAttack(battle: Battle, character: BattleCharacter) {
+    const b = characterPerformAttack(battle, character.character.id);
+    setBattle((old) => ({
+      characters: old.characters,
+      rounds: [...b.rounds],
+      events: [...b.events],
     }));
   }
 
@@ -41,51 +61,82 @@ export default function Battle() {
     currentCharacterTurn: BattleCharacter
   ) {
     const { character } = battleChar;
-    const npcClass = !character.isPlayerControlled
-      ? "border-rose-400"
-      : "border-blue-400";
-
-    const hasSpentAction = characterHasSpentAction(battle, character);
+    const hasSpentAction = characterHasSpentPrimaryAction(battle, character);
+    const hasSpentBonus = characterHasSpentBonusAction(battle, character);
+    const hasFinished = characterHasFinishedRound(battle, character);
 
     const currentAction =
       battleChar.character.id ===
       (currentCharacterTurn ? currentCharacterTurn.character.id : undefined);
 
     const currentActionClass = currentAction
-      ? "shadow-[0_0px_35px_0px_rgba(0,0,0,0.3)] shadow-red-600"
+      ? "shadow-[0_0px_40px_0px_rgba(0,0,0,0.3)] shadow-red-600"
       : "";
 
     return (
       <div
         key={character.id}
-        className={`flex justify-between border w-full p-4 shadow-md ${
-          npcClass || ""
-        } ${currentActionClass || ""}`}
+        className={`flex justify-between border border-slate-700 w-full overflow-hidden shadow-md relative ${
+          currentActionClass || ""
+        }`}
       >
-        <div>
-          <div>{character.name}</div>
-          <div>{character.race}</div>
-          <div>{battleChar.initiative}</div>
+        <div
+          className="absolute w-full h-full z-0"
+          style={{
+            backgroundImage: `linear-gradient(90deg, rgba(0, 0, 0, 1) 200px, rgba(0, 0, 0, 0) 150%), url(${character.imageUrl})`,
+            backgroundSize: `cover, cover`,
+            backgroundPositionX: `0, 200px`,
+            color: "#000",
+          }}
+        ></div>
+
+        <div className="flex justify-between w-full relative p-4">
           <div>
-            Level {character.characterClasses[0].level}{" "}
-            {character.characterClasses[0].clazz}
+            <div>{character.name}</div>
+            <div>{character.race}</div>
+            <div>{battleChar.initiative}</div>
+            <div>
+              Level {character.characterClasses[0].level}{" "}
+              {character.characterClasses[0].clazz}
+            </div>
           </div>
-        </div>
-        <div>
-          <button
-            disabled={hasSpentAction}
-            onClick={() => performAttack(battle, battleChar)}
-            className="border disabled:border-slate-500 disabled:bg-slate-700 disabled:text-slate-500 border-white p-3 rounded hover:bg-slate-500"
-          >
-            Attack
-          </button>
+
+          <div className="flex gap-x-2">
+            <button
+              disabled={hasSpentAction || hasFinished}
+              onClick={() => performAttack(battle, battleChar)}
+              className="border disabled:border-slate-500 disabled:bg-slate-700 disabled:text-slate-500 border-white p-3 rounded hover:bg-slate-500"
+            >
+              Action
+            </button>
+
+            <button
+              disabled={hasSpentBonus || hasFinished}
+              onClick={() => performBonus(battle, battleChar)}
+              className="border disabled:border-slate-500 disabled:bg-slate-700 disabled:text-slate-500 border-white p-3 rounded hover:bg-slate-500"
+            >
+              Bonus action
+            </button>
+
+            <button
+              disabled={hasFinished}
+              onClick={() => finishRound(battle, battleChar)}
+              className="border disabled:border-slate-500 disabled:bg-slate-700 disabled:text-slate-500 border-white p-3 rounded hover:bg-slate-500"
+            >
+              End
+            </button>
+          </div>
         </div>
       </div>
     );
   }
 
-  function renderAction(action: BattleCharacterAction) {
-    return <div key={action.characterId}>{action.action.type}</div>;
+  function renderAction(event: BattleEvent) {
+    return (
+      <div key={event.characterId}>
+        {event.eventType} {event.actionType}
+      </div>
+    );
   }
 
   function addCharacter(isPlayerControlled: boolean) {
@@ -96,6 +147,7 @@ export default function Battle() {
           ...battle.characters,
           {
             character: {
+              imageUrl: "/character.png",
               id: Date.now().toString(),
               isPlayerControlled: isPlayerControlled,
               name: isPlayerControlled ? "New character" : "Monster",
@@ -160,33 +212,36 @@ export default function Battle() {
 
   return (
     <div className="w-full">
-      <div className="flex gap-x-2 mb-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => addCharacter(true)}
-        >
-          Add character
-        </button>
+      <div className="flex mb-4 justify-between">
+        <div className="flex gap-x-2">
+          <button
+            className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => addCharacter(true)}
+          >
+            Add character
+          </button>
 
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => addCharacter(false)}
-        >
-          Add monster
-        </button>
-
-        <button
-          disabled={hasRolledForInitiative(battle)}
-          className="bg-green-500 disabled:bg-green-900 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => initiativeRoll()}
-        >
-          Roll initiative
-        </button>
+          <button
+            className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => addCharacter(false)}
+          >
+            Add monster
+          </button>
+        </div>
+        <div>
+          <button
+            disabled={hasRolledForInitiative(battle)}
+            className="bg-green-500 disabled:bg-green-900 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => initiativeRoll()}
+          >
+            Roll initiative
+          </button>
+        </div>
       </div>
 
       <h1 className="text-xl mb-4">Round {battle.rounds.length}</h1>
       <div className="flex w-full gap-x-4 mb-4">
-        {battle.rounds[battle.rounds.length - 1].characterActions.map(
+        {getRoundEvents(battle.rounds[battle.rounds.length - 1], battle).map(
           renderAction
         )}
       </div>

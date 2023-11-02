d85ffaeaeaa64e405672168cc312e5d9156f36a3
Maximilian Paju <maximilian.paju@gmail.com>
2023-11-02 11:26:17 +0100

    feat: battle events

--- src/lib/dnd/battle/battle.ts
+++ src/lib/dnd/battle/battle.ts
@@ -1,5 +1,7 @@
 import { Character } from "../character/character";
 
+type Id = string;
+
 export type BattleCharacter = {
   character: Character;
   initiative: number;
@@ -11,34 +13,112 @@ export type BattleCharacterAction = {
 };
 
 export type BattleRound = {
-  characterActions: BattleCharacterAction[];
+  id: Id;
 };
 
-export type BattleAction = {
-  action: "primary" | "secondary";
-  type: "attack" | "move";
+export type BattleEvent = {
+  characterId: Id;
+  roundId: Id;
+  eventType: BattleEventType;
+  actionType: ActionType;
 };
+
+type BattleEventType =
+  | "start"
+  | "primary-action"
+  | "secondary-action"
+  | "movement"
+  | "end";
+
+type ActionType =
+  | "attack"
+  | "sprint"
+  | "jump"
+  | "cantrip"
+  | "spell"
+  | "item"
+  | "equipment"
+  | "none";
+
+export type BattleAction = {};
 
 export type Battle = {
   characters: BattleCharacter[];
   rounds: BattleRound[];
+  events: BattleEvent[];
 };
 
 export function hasRolledForInitiative(battle: Battle) {
-  return battle.characters.every((c) => c.initiative > 0);
+  return battle.characters.every((c) => c.initiative != 0);
 }
 
-export function characterHasSpentAction(battle: Battle, character: Character) {
+export function getRoundEvents(round: BattleRound, battle: Battle) {
+  return battle.events.filter((e) => e.roundId === round.id);
+}
+
+export function getCharacterRoundEvents(
+  battle: Battle,
+  round: BattleRound,
+  character: Character
+) {
+  const roundEvents = getRoundEvents(round, battle);
+  return roundEvents.filter((re) => re.characterId === character.id);
+}
+
+export function characterHasSpentAction(
+  battle: Battle,
+  character: Character,
+  eventType: BattleEventType
+) {
   const round = getCurrentRound(battle);
-  return round.characterActions.some((c) => c.characterId === character.id);
+  const roundCharacterEvents = getCharacterRoundEvents(
+    battle,
+    round,
+    character
+  );
+
+  return roundCharacterEvents.some(
+    (c) => c.characterId === character.id && c.eventType === eventType
+  );
+}
+
+export function characterHasSpentPrimaryAction(
+  battle: Battle,
+  character: Character
+) {
+  return characterHasSpentAction(battle, character, "primary-action");
+}
+
+export function characterHasSpentBonusAction(
+  battle: Battle,
+  character: Character
+) {
+  return characterHasSpentAction(battle, character, "secondary-action");
+}
+
+export function characterHasFinishedRound(
+  battle: Battle,
+  character: Character
+) {
+  return characterHasSpentAction(battle, character, "end");
+}
+
+export function characterHasMoved(battle: Battle, character: Character) {
+  return characterHasSpentAction(battle, character, "movement");
 }
 
 export function currentCharacterTurn(battle: Battle) {
   const charactersNotActedCurrentRound = battle.characters.filter(
     (battleChar) => {
-      const currentRound = battle.rounds.slice(-1)[0];
-      const hasActed = currentRound.characterActions.some(
-        (c) => c.characterId === battleChar.character.id
+      const currentRound = getCurrentRound(battle);
+      const currentRoundEvents = getCharacterRoundEvents(
+        battle,
+        currentRound,
+        battleChar.character
+      );
+      const hasActed = currentRoundEvents.some(
+        (e) =>
+          e.characterId === battleChar.character.id && e.eventType === "end"
       );
 
       return !hasActed;
@@ -56,15 +136,40 @@ export function getCurrentRound(battle: Battle) {
   return battle.rounds[battle.rounds.length - 1];
 }
 
-export function characterAttack(battle: Battle, characterId: string) {
+export function characterFinished(battle: Battle, characterId: string) {
   const currentRound = getCurrentRound(battle);
 
-  currentRound.characterActions.push({
-    action: {
-      action: "primary",
-      type: "attack",
-    },
+  battle.events.push({
+    eventType: "end",
+    actionType: "none",
     characterId: characterId,
+    roundId: currentRound.id,
+  });
+
+  return battle;
+}
+
+export function characterPerformBonus(battle: Battle, characterId: string) {
+  const currentRound = getCurrentRound(battle);
+
+  battle.events.push({
+    actionType: "sprint",
+    eventType: "secondary-action",
+    characterId: characterId,
+    roundId: currentRound.id,
+  });
+
+  return battle;
+}
+
+export function characterPerformAttack(battle: Battle, characterId: string) {
+  const currentRound = getCurrentRound(battle);
+
+  battle.events.push({
+    actionType: "attack",
+    eventType: "primary-action",
+    characterId: characterId,
+    roundId: currentRound.id,
   });
 
   return battle;

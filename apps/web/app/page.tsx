2cfc5531c62b64ba68ad9e46c003dde5b965d55f
Maximilian Paju <maximilian.paju@gmail.com>
2024-03-04 15:42:54 +0100

    chore: production warning

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,6 +7,16 @@ export default function Home() {
   return (
     <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
       <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+        <div className="text-left">
+          <Alert>
+            <AlertTriangle className="h-4 w-4" />
+            <AlertTitle>Production is read-only</AlertTitle>
+            <AlertDescription>
+              Production is read-only, come back later.
+            </AlertDescription>
+          </Alert>
+        </div>
+
         <h1 className="text-3xl font-semibold sm:text-5xl md:text-6xl lg:text-7xl">
           Roleplaying campaign manager
         </h1>
@@ -21,14 +31,6 @@ export default function Home() {
             Play demo
           </ButtonLink>
         </div>
-
-        <Alert>
-          <AlertTriangle className="h-4 w-4" />
-          <AlertTitle>Production is read-only</AlertTitle>
-          <AlertDescription>
-            Production is read-only, come back later.
-          </AlertDescription>
-        </Alert>
       </div>
     </section>
   );

faae96766513fc63488c0ee4cf96781157d298f1
Maximilian Paju <maximilian.paju@gmail.com>
2024-03-05 09:33:31 +0100

    chore: world card styling

--- apps/web/components/world-card.tsx
+++ apps/web/components/world-card.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import { World } from "@repo/rp-lib";
 import { H3, Muted } from "./ui/typography";
-import { Separator } from "./ui/separator";
 
 type WorldCardProps = { world: World };
 
@@ -12,22 +11,18 @@ export function WorldCard({ world }: WorldCardProps) {
     <Link href={`/worlds/${world.id}`}>
       <div className="border border-slate-500 p-3 hover-border-slate-100">
         <H3>{world.name}</H3>
-        <div>
+        <Muted>
           {worldData.rounds.length > 0 && (
             <>Played for {worldData.rounds.length} rounds</>
           )}
-          {worldData.rounds.length === 0 && <Muted>Not started</Muted>}
-        </div>
-        <div>
+          {worldData.rounds.length === 0 && <>Not started</>}
+        </Muted>
+        <Muted>
           {worldData.characters.length > 0 && (
             <>{worldData.characters.length} characters</>
           )}
-          {worldData.characters.length === 0 && (
-            <Muted>No character created yet</Muted>
-          )}
-        </div>
-        <Separator className="my-2" />
-        <div>Version {world.libVersion}</div>
+          {worldData.characters.length === 0 && <>No character created yet</>}
+        </Muted>
       </div>
     </Link>
   );

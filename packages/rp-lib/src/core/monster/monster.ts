bf9859b073cb8329f6d19c8b7a06af53f57d96cb
Maximilian Paju <maximilian.paju@gmail.com>
2024-03-06 20:41:13 +0100

    chore: add monster properties

--- packages/rp-lib/src/core/monster/monster.ts
+++ packages/rp-lib/src/core/monster/monster.ts
@@ -1,11 +1,27 @@
-import { World } from "../..";
+import { Interaction, World } from "../..";
 import { Id } from "../../lib/generate-id";
+import { CharacterResource, Position } from "../character/character";
+import { Status } from "../interaction/status";
 
 export class Monster {
   id!: Id;
   name!: string;
+  actions!: Interaction[];
+  party!: Id;
+  imageUrl!: string;
+  baseArmorClass!: number;
+  armorClass!: number;
+  statuses!: Status[];
+  position!: Position;
 
-  public constructor(world: World, init?: Partial<Monster>) {
+  maximumHealth!: number;
+  currentHealth!: number;
+  temporaryHealth!: number;
+
+  resourcesCurrent!: CharacterResource[];
+  resourcesMax!: CharacterResource[];
+
+  constructor(world: World, init?: Partial<Monster>) {
     Object.assign(this, init);
   }
 }

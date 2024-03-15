3dd51cc4857c511b87f1dbe0d6422a0c7ef3a408
Maximilian Paju <maximilian.paju@gmail.com>
2024-03-15 13:49:42 +0100

    chore: card hover effect

--- apps/web/components/ui/card.tsx
+++ apps/web/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm transition-all hover:bg-foreground/10",
       className
     )}
     {...props}

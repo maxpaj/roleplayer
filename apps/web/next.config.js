098af26c8842dadf8e208b7d32870f48b2829f9f
Maximilian Paju <maximilian.paju@gmail.com>
2024-03-15 17:17:14 +0100

    chore: remove domains from next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["https://roleplayer.maxpaj.com"],
     remotePatterns: [
       {
         protocol: "https",

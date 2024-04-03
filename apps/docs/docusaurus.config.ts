3a0854a4298dd117f5cf145888919dd7ad93a45b
Maximilian Paju <maximilian.paju@gmail.com>
2024-04-03 18:32:41 +0200

    chore: add algolia

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -66,6 +66,31 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      appId: "WNDY4ZZ3VN",
+      apiKey: "7803cc5dad49fdec71ef2e41cd9a2e0d",
+      indexName: "roleplayer-maxpaj",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      //... other Algolia params
+    },
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {

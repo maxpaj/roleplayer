d6dcefc0f752d5216a27081684155e05aa996bb6
Maximilian Paju <maximilian.paju@gmail.com>
2025-01-15 13:49:33 +0100

    feat: get ruleset

--- packages/roleplayer/src/data/index.ts
+++ packages/roleplayer/src/data/index.ts
@@ -1,2 +1,16 @@
+import { DnDRuleset } from "./rulesets/dnd-5th";
+import { LeagueOfDungeoneersRuleset } from "./rulesets/league-of-dungeoneers";
+
 export * from "./rulesets/dnd-5th";
 export * from "./rulesets/league-of-dungeoneers";
+
+export function getRuleSet(ruleset: string) {
+  switch (ruleset) {
+    case "DnD5th":
+      return new DnDRuleset();
+    case "LeagueOfDungeoneers":
+      return new LeagueOfDungeoneersRuleset();
+    default:
+      throw new Error("Unknown ruleset");
+  }
+}
